ebcd87a98bff12e6d5bb4f8d66a3762443ecad12
dont-way-56789111337 <maxsvasilyev909@gmail.com>
2025-03-31 17:05:00 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// // Popup basket
+
+// let basketContentButtonItemsButton = document.querySelector('.basket-content_button_items_button');
+// let popup = document.querySelector('.popup');
+
+// basketContentButtonItemsButton.onclick = function() {
+//     popup.style.display = 'flex';
+// }
+
+// window.onclick = function(event) {
+//     if (event.target === popup) {
+//         popup.style.display = 'none';
+//     }
+// }
